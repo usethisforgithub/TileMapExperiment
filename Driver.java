eb0adedfe50292a784a712428990e4814d02e6b4
usethisforgithub <usethisforgithub@users.noreply.github.com>
2017-07-30 21:59:00 -0500

    fixed

--- Driver.java
+++ Driver.java
@@ -1,7 +1,16 @@
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Driver {
-	public static void main(String[] args){
-		ScreenWindow window = new ScreenWindow();
-		new Thread(window).start();
+	
+	public static void main(String[] args) {
+		
+			MenuWindow window = new MenuWindow();
+			new Thread(window).start();
+		
 	}
 }
